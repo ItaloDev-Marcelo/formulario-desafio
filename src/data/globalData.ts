79e1003f2ccc067c9f4b99d58d0136d6de5ce506
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-06-19 18:57:39 -0300

    Correção no zod

--- src/data/globalData.ts
+++ src/data/globalData.ts
@@ -46,35 +46,35 @@ export const InputRadioData0 :RadioFormate[] =  [
     key: 0,
     value: 'frontend',
     type: 'radio',
-    classes: 'flex cursor-pointer py flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
+    classes: 'flex cursor-pointer py-1 flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
     inputclass: 'hidden'
   },
   {
     key: 1,
     value: 'mobile',
     type: 'radio',
-    classes: 'flex cursor-pointer py flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
+    classes: 'flex cursor-pointer py-1 flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
    inputclass: 'hidden'
   },
   {
     key: 2,
     value: 'backend',
     type: 'radio',
-    classes: 'flex cursor-pointer py flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
+    classes: 'flex cursor-pointer py-1 flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
     inputclass: 'hidden'
   },
   {
     key: 3,
     value: 'fullstack',
     type: 'radio',
-    classes: 'flex cursor-pointer py flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
+    classes: 'flex cursor-pointer py-1 flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
     inputclass: 'hidden'
   },
   {
     key: 4,
     value: 'devops',
     type: 'radio',
-    classes: 'flex cursor-pointer py flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
+    classes: 'flex cursor-pointer py-1 flex-row border-2 items-start w-[100%] h-[40px] md:w-[180] rounded-[6px]',
     inputclass: 'hidden'
   }
  ]
--- src/lib/zodSchema.tsx
+++ src/lib/zodSchema.tsx
@@ -2,11 +2,11 @@ import { z } from "zod";
  
 
  export const schema = z.object({
-     nome: z.string().min(7).nonempty('Informe seu nome completo'),
+     nome: z.string().nonempty('Informe seu nome completo'),
      email: z.string().email('Informe seu Email'),
      telefone: z.string().nonempty('Informe um numero valido') ,
-     linkedin: z.string().nonempty('Informe seu linkedin'),
-     radioType: z.string().nonempty('Informe seu um campo')
+     linkedin: z.string().nonempty('Informe seu linkedin').url('URL Inválida'),
+     radioType: z.string({required_error: "Selecione uma Stack", invalid_type_error: "Selecione uma Stack"}).nonempty('Informe seu um campo')
   })
 
   export type FormData  = z.infer<typeof schema>;
